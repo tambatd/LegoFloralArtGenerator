c354bbb05db484413c9bf20cca3c37d741d53286
Devendra <tambatd@outlook.com>
2023-05-22 01:40:54 -0400

    next configs updated

--- next.config.js
+++ next.config.js
@@ -2,13 +2,3 @@
 const nextConfig = {};
 
 module.exports = nextConfig;
-
-// next.config.js
-const isProd = process.env.NODE_ENV === "production";
-
-module.exports = {
-  assetPrefix: isProd ? "/LegoFloralArtGenerator/" : "",
-  images: {
-    unoptimized: true,
-  },
-};
